3d244988259e35861941fc486f9220a300d907d2
ivanovichi <ivan.arthur18@gmail.com>
2026-07-23 16:13:56 -0600

    feat: Page finished

--- server/index.js
+++ server/index.js
@@ -1,85 +1,112 @@
 import http from 'node:http';
 import fs from 'node:fs/promises';
+import { URL } from 'node:url';
 
-const server = http.createServer((req,res) => {
+const server = http.createServer(async (req,res) => {
     console.log(req.method);
     console.log(req.url);
+    const theTest = req.url;
+    let theURL = new URL("http:localhost:8000" + req.url);
 
-    if(req.url === '/index' || req.url === '/') {
-        async function info () {
-            try {
-                let data = await fs.readFile('../pages/index.html', {encoding: 'utf8'});
-                res.writeHead(200, {
-                    'content-type': 'text/html'
-                });
-                res.end(data);
-            } catch (err) {
-                console.error(err);
-                res.writeHead(500, {
-                    'content-type': 'text/plain'
-                });
-                res.end('Oh no');
-            }      
-        }
-        info();
+    if(theTest.match(/[/].*[/]/) === null) {
+        let responseInfo = await htmlInfo(theURL.pathname);
+        res.writeHead(responseInfo[0], {
+            'content-type': responseInfo[1]
+        });
+        res.end(responseInfo[2]);
         return;
     }
 
-    if(req.url === '/styles/index-style.css') {
-        async function info() {
-            try {
-                let styleData = await fs.readFile('../styles/index-style.css', {encoding: 'utf8'});
-                res.writeHead(200, {
-                    'content-type': 'text/css'
-                });
-                res.end(styleData);
-            } catch (err) {
-                console.error(err);
-                res.writeHead(500, {
-                    'content-type': 'text/plain'
-                });
-                res.end('Oh no');
-            }
-        }
-        info();
+    if(theTest.match(/[/]styles[/]/) !== null) {
+        let responseInfo = await styleInfo(theURL.pathname);
+        res.writeHead(responseInfo[0], {
+            'content-type': responseInfo[1]
+        });
+        res.end(responseInfo[2]);
         return;
     }
 
-    if(req.url === '/img/Logo.png?v=2' || req.url === '/img/Logo.png') {
-        async function info() {
-            try{ 
-                let logoData = await fs.readFile('../img/Logo.png');
-                res.writeHead(200, {
-                    'content-type': 'image/png'
-                });
-                res.end(logoData);
-            } catch (err) {
-                console.error(err);
-                res.writeHead(500, {
-                    'content-type': 'text/plain'
-                });
-                res.end('Oh no');
-            }
-        }
-        info();
+    if(theTest.match(/[/]img[/]/) !== null) {
+        let responseInfo = await imageInfo(theURL.pathname);
+        res.writeHead(responseInfo[0], {
+            'content-type': responseInfo[1]
+        });
+        res.end(responseInfo[2]);
         return;
     }
 
-
-
-
-    if(req.url === '/about') {
-        res.end('<h1>About</h1>');
-        return;
-    }
-
-    if(req.url === '/contact') {
-        res.end('<h1>Contact</h1>');
-        return;
-    }
-
-    res.writeHead(404);
-    res.end('<h1>404</h1>');
+    let notFoundInfo = await notFound();
+    res.writeHead(notFoundInfo[0], {
+        'content-type': notFoundInfo[1]
+    });
+    res.end(notFoundInfo[2]);
 });
+
+async function htmlInfo(file) {
+    if(file === "/") {
+        file = "/index";
+    }
+
+    try {
+        let data = await fs.readFile(`../pages${file}.html`, {encoding: 'utf8'});
+        return [200, 'text/html', data];
+    } catch (err) {
+        if(err.code === "ENOENT") {
+            try {
+                let notFound = await fs.readFile("../pages/404.html", {encoding: 'utf8'});
+                return [404, 'text/html', notFound];
+            }
+            catch (err2) {
+                console.error(err2);
+                return [500, 'text/plain', 'Oh no'];
+            }
+        } else {
+            console.error(err);
+            return [500, 'text/plain', 'Oh no'];
+        }
+    };
+};
+
+async function styleInfo(file) {
+    try {
+        let styleData = await fs.readFile(`..${file}`, {encoding: 'utf8'});
+        return [200, 'text/css', styleData];
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            return [404, "text/plain", "Not Found"];
+        }
+        console.error(err);
+        return [500, "text/plain", 'Oh no'];
+    };
+};
+
+async function imageInfo(file) {
+    try {
+        let imageData = await fs.readFile(`..${file}`);
+        if(/svg$/ !== null) {
+            return [200, 'image/svg+xml', imageData];
+        } else {
+            return [200, 'image/png', imageData];
+        }
+
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            return [404, "text/plain", "Not Found"];
+        }
+        console.error(err);
+        return [500, "text/plain", 'Oh no'];
+    };
+};
+
+async function notFound() {
+    try {
+        let errorData = await fs.readFile("../pages/404.html");
+        return [404, 'text/html', errorData];
+    } catch (err) {
+        console.error(err);
+        return[500, 'text/plain', 'Oh no'];
+    }
+}
+
 
 server.listen(8000);
